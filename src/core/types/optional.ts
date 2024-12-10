bc37e1e60b279dcce4a6927002648205e0c6b76c
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-12-10 16:47:31 -0400

    docs: add explanation about `types/optional.ts`

--- src/core/types/optional.ts
+++ src/core/types/optional.ts
@@ -1,6 +1,11 @@
 // isso permite você passar um tipo e depois informar qual das chaves do 
 // objeto de tipagem você quer que virem opcionais
 
+// O que é Optional?
+// Ele recebe dois parâmetros genéricos:
+// T: O tipo base que você está trabalhando (por exemplo, Post).
+// K: As chaves do tipo T que você quer tornar opcionais.
+
 // 1. Pick<Partial<T>, K>
 // Partial<T> transforma todas as propriedades do tipo T em opcionais.
 // Pick<...> seleciona apenas as propriedades de T que estão no conjunto K (as que você quer tornar opcionais).
@@ -12,6 +17,12 @@
 // Combina o resultado:
 // As propriedades em K agora são opcionais.
 // As demais propriedades de T permanecem como estavam (obrigatórias).
+
+// ou seja, Pick<Partial<T>, K> está tornando todas as props do objeto opcionais e selecionando as K 
+// propriedades que você quer que sejam opcionais 
+
+// Omit<T, K> pega o seu objeto inicial e retira dele as props K que você selecionou para serem
+// opcionais. O que restar em T permanece como opcional
 
 /**
  * Make some property optional on type
