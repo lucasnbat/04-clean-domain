4e05d4350a890f5de7acfa58addbb4ffb8f61ab6
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-12-13 09:06:53 -0400

    feat: add fetch question answers use case with tests

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
@@ -0,0 +1,53 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { FetchQuestionAnswersUseCase } from './fetch-question-answers'
+import { makeAnswer } from 'test/factories/make-answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryAnswersRepositoryInstance: InMemoryAnswersRepository
+let sut: FetchQuestionAnswersUseCase
+
+describe('Fetch Question Answers', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepositoryInstance = new InMemoryAnswersRepository()
+
+    sut = new FetchQuestionAnswersUseCase(inMemoryAnswersRepositoryInstance)
+  })
+
+  it('should be able to fetch question answers', async () => {
+    await inMemoryAnswersRepositoryInstance.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+    await inMemoryAnswersRepositoryInstance.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+    await inMemoryAnswersRepositoryInstance.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+
+    const { answers } = await sut.execute({ questionId: 'question-1', page: 1 })
+
+    expect(answers).toHaveLength(3)
+  })
+
+  it('should be able to fetch paginated recent questions', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryAnswersRepositoryInstance.create(
+        makeAnswer({
+          questionId: new UniqueEntityID('question-1'),
+        }),
+      )
+    }
+
+    // pedindo por página 2...
+    const { answers } = await sut.execute({ questionId: 'question-1', page: 2 })
+
+    // espera-se que só liste 2 registros
+    expect(answers).toHaveLength(2)
+  })
+})
