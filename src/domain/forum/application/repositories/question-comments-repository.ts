299a8b01762e41c433c28585194e14145246945c
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-12-13 11:29:47 -0400

    feat: add delete comment of question use case with tests

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -2,4 +2,6 @@ import { QuestionComment } from '../../enterprise/entities/question-comment'
 
 export interface QuestionCommentsRepository {
   create(question: QuestionComment): Promise<void>
+  findById(id: string): Promise<QuestionComment | null>
+  delete(questionComment: QuestionComment): Promise<void>
 }
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -4,6 +4,24 @@ import { QuestionComment } from '@/domain/forum/enterprise/entities/question-com
 export class InMemoryQuestionCommentsRepository
   implements QuestionCommentsRepository
 {
+  async findById(id: string) {
+    const questionComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!questionComment) {
+      return null
+    }
+
+    return questionComment
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
+  }
+
   public items: QuestionComment[] = []
 
   async create(questionComment: QuestionComment) {
