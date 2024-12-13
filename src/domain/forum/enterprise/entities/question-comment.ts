0fe863bc0d7bd8c40991cfa5a8f08b141aae26ae
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-12-13 09:18:34 -0400

    feat: add question and answer comment entities

---
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -0,0 +1,53 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+export interface QuestionCommentProps {
+  authorId: UniqueEntityID
+  questionId: UniqueEntityID
+  content: string
+  createdAt: Date
+  updatedAt?: Date | null
+}
+
+export class QuestionComment extends Entity<QuestionCommentProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  // método criado para vigiar e setar datas de alteração de informação
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  static create(
+    props: Optional<QuestionCommentProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const questionComment = new QuestionComment(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(), // sistema gera autpmático caso não tenha createdAt informado
+      },
+      id,
+    )
+    return questionComment
+  }
+}
