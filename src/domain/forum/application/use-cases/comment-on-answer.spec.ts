5b629c989764cda2d46b686fea73005248694c1a
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-12-13 11:06:21 -0400

    feat: add comment on answer use case with test

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,37 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository copy'
+
+let inMemoryAnswersRepositoryInstance: InMemoryAnswersRepository
+let inMemoryAnswerCommentsRepositoryInstance: InMemoryAnswerCommentsRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment On Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepositoryInstance = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentsRepositoryInstance =
+      new InMemoryAnswerCommentsRepository()
+
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepositoryInstance,
+      inMemoryAnswerCommentsRepositoryInstance,
+    )
+  })
+
+  it('should be able to comment on answer', async () => {
+    const answer = makeAnswer()
+
+    await inMemoryAnswersRepositoryInstance.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: 'Comentário teste',
+    })
+
+    expect(inMemoryAnswerCommentsRepositoryInstance.items[0].content).toEqual(
+      'Comentário teste',
+    )
+  })
+})
