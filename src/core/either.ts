d1377617e35bd2f2eb15479e22a30138b08d12fc
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-12-17 14:39:45 -0400

    refactor: ajust Either class including auxiliar methods

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,24 +1,42 @@
 // Error
-export class Left<L> {
+export class Left<L, R> {
   readonly value: L
 
   constructor(value: L) {
     this.value = value
   }
+
+  // métodos auxiliares criados durante os testes da classe
+  isRight(): this is Right<L, R> {
+    return false
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true
+  }
 }
 
 // Success
-export class Right<R> {
+export class Right<L, R> {
   readonly value: R
 
   constructor(value: R) {
     this.value = value
   }
+
+  // métodos auxiliares criados durante os testes da classe
+  isRight(): this is Right<L, R> {
+    return true
+  }
+
+  isLeft(): this is Left<L, R> {
+    return false
+  }
 }
 
 // criando tipagem que pode apontar para classes Left ou Right
 // Associa cada classe a uma letra e tipa as classes que tão acima
-export type Either<L, R> = Left<L> | Right<R>
+export type Either<L, R> = Left<L, R> | Right<L, R>
 
 // funções que vão instanciar as classes (poderiam ser métodos static tb)
 // depois temos a tipagem de valores de entrada <L, R>
