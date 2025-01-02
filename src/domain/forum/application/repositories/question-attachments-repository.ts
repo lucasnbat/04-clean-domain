ba363cfc3ad05721c9865f2deb37def71a787ff0
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2025-01-02 10:53:34 -0400

    refactor: ajust to edit question use case

--- src/domain/forum/application/repositories/question-attachments-repository.ts
+++ src/domain/forum/application/repositories/question-attachments-repository.ts
@@ -1,12 +1,5 @@
-import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
 
 export interface QuestionAttachmentsRepository {
-  create(answer: QuestionAttachment): Promise<void>
-  findById(id: string): Promise<QuestionAttachment | null>
-  findManyByAnswerId(
-    answerId: string,
-    params: PaginationParams,
-  ): Promise<QuestionAttachment[]>
-  delete(questionAttachment: QuestionAttachment): Promise<void>
+  findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]>
 }
--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -3,6 +3,10 @@ import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
 import { NotAllowedError } from './errors/not-allowed-error'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { QuestionAttachmentsRepository } from '../repositories/question-attachments-repository'
+import { QuestionAttachmentList } from '../../enterprise/entities/question-attachment-list'
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 interface EditQuestionUseCaseRequest {
   questionId: string
@@ -20,7 +24,10 @@ type EditQuestionUseCaseResponse = Either<
 >
 
 export class EditQuestionUseCase {
-  constructor(private questionsRepository: QuestionsRepository) {}
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private questionAttachmentsRepository: QuestionAttachmentsRepository,
+  ) {}
 
   async execute({
     questionId,
@@ -41,9 +48,33 @@ export class EditQuestionUseCase {
       return left(new NotAllowedError())
     }
 
+    // busca todos anexos atuais da pergunta
+    const currentQuestionAttachments =
+      await this.questionAttachmentsRepository.findManyByQuestionId(questionId)
+
+    // cria lista de attachemnts/anexos no molde da QuestionAttachamentList
+    const questionAttachmentList = new QuestionAttachmentList(
+      currentQuestionAttachments,
+    )
+
+    // map para gerar os anexos novos, recebidos agora, no formato da
+    // QuestionAttachment
+    const questionAttachments = attachmentsIds.map((attachmentsId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentsId),
+        questionId: question.id,
+      })
+    })
+
+    // pega a lista atual com suas instancias de QuestionAttachment
+    // e atualiza inserindo os QuestionAttachments recebidos
+    questionAttachmentList.update(questionAttachments)
+
     // só altera campos que tem set() na classe Question
     question.title = title
     question.content = content
+    // seta a lista atualizada de attachments no atributo da question
+    question.attachments = questionAttachmentList
 
     await this.questionsRepository.save(question)
 
