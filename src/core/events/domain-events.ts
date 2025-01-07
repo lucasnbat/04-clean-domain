84e2bcda102a7e03996287a1e14f56e9d217cba9
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2025-01-07 15:05:01 -0400

    docs: working on domain events lessons

---
+++ src/core/events/domain-events.ts
@@ -0,0 +1,98 @@
+import { AgregateRoot } from '../entities/agregate-root'
+import { UniqueEntityID } from '../entities/unique-entity-id'
+
+import { DomainEvent } from './domain-event'
+
+type DomainEventCallback = (event: any) => void
+
+export class DomainEvents {
+  // handlersMap tem o nome do evento (string) e a função que será chamada
+  // ao checar o nome do evento (ela sera do tipo DomainEventCallback)
+  // É um vetor de funções do tipo DomainEventCallback porque posso ter vários
+  // subscribers com várias funções que podem ser chamadas dependendo do
+  // evento (tipo string) que é detectado
+  private static handlersMap: Record<string, DomainEventCallback[]> = {}
+
+  // marca quais agregados da aplicação tem eventos pendentes (ex: answers
+  // que ainda já salvas no BD e que estão com ready = true )
+  private static markedAggregates: AgregateRoot<any>[] = []
+
+  public static markAggregateForDispatch(aggregate: AgregateRoot<any>) {
+    // se não achou o agregado com id passado na lista, agreggateFound = false
+    const aggregateFound = !!this.findMarkedAggregateByID(aggregate.id)
+
+    // !false = true -> agregado sera adicionado na lista de eventos pendentes
+    if (!aggregateFound) {
+      this.markedAggregates.push(aggregate)
+    }
+  }
+
+  // método que será chamado pelo banco de dados para fazer o ready ficar
+  // true e fazer o subscriber disparar o evento de notificação, por ex
+  private static dispatchAggregateEvents(aggregate: AgregateRoot<any>) {
+    aggregate.domainEvents.forEach((event: DomainEvent) => this.dispatch(event))
+  }
+
+  private static removeAggregateFromMarkedDispatchList(
+    aggregate: AgregateRoot<any>,
+  ) {
+    const index = this.markedAggregates.findIndex((a) => a.equals(aggregate))
+
+    this.markedAggregates.splice(index, 1)
+  }
+
+  // função utilitaria para achar id do agreggate marcado como pendente
+  private static findMarkedAggregateByID(
+    id: UniqueEntityID,
+  ): AgregateRoot<any> | undefined {
+    return this.markedAggregates.find((aggregate) => aggregate.id.equals(id))
+  }
+
+  public static dispatchEventsForAggregate(id: UniqueEntityID) {
+    const aggregate = this.findMarkedAggregateByID(id)
+
+    if (aggregate) {
+      this.dispatchAggregateEvents(aggregate)
+
+      aggregate.clearEvents()
+
+      this.removeAggregateFromMarkedDispatchList(aggregate)
+    }
+  }
+
+  public static register(
+    callback: DomainEventCallback,
+
+    eventClassName: string,
+  ) {
+    const wasEventRegisteredBefore = eventClassName in this.handlersMap
+
+    if (!wasEventRegisteredBefore) {
+      this.handlersMap[eventClassName] = []
+    }
+
+    this.handlersMap[eventClassName].push(callback)
+  }
+
+  public static clearHandlers() {
+    this.handlersMap = {}
+  }
+
+  public static clearMarkedAggregates() {
+    this.markedAggregates = []
+  }
+
+  private static dispatch(event: DomainEvent) {
+    const eventClassName: string = event.constructor.name
+
+    const isEventRegistered = eventClassName in this.handlersMap
+
+    if (isEventRegistered) {
+      const handlers = this.handlersMap[eventClassName]
+
+      for (const handler of handlers) {
+        handler(event)
+      }
+    }
+  }
+}
